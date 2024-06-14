3fd25bed768bb7eaf0e4dab205a07849b2a0f3e0
Syed-Mohd-Azam <smazamamu@gmail.com>
2024-06-15 00:27:45 +0530

    Changes added to the Application

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
-
 function App() {
   return (
     <>
